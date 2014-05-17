d075c737f4179599b37c659ded46fdf18ca42e37
GeekyGamer14 <GeekyGamer14@iQuestria.net>
2014-05-16 21:30:08 -0700

    honestly i only made this for the name

---
+++ ajaxulate.js
@@ -0,0 +1,26 @@
+ajaxulate = {
+	post: function(url, data, callback){
+		$.ajax({
+			url: url,
+			type: "POST",
+			data: data,
+			success: callback,
+			error: callback,
+			done: callback
+		});
+	},
+	get: function(url, data, callback){
+		$.ajax({
+			url: url,
+			type: "GET",
+			data: data,
+			success: callback,
+			error: callback,
+			done: callback
+		});
+	}
+};
+
+if(!jQuery){
+	throw "jQuery was not detected, ajaxulate will not work correctly";
+}
